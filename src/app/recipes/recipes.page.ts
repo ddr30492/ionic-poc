72438878dccb2c5cdc022475a1f2ce95f306c6e5
ddr30492 <deepdarji6@gmail.com>
2021-09-08 00:04:45 +0530

    crate recipe component and adding one model for item, use ngfor loop and call that mnodel in component as a string object

--- src/app/recipes/recipes.page.ts
+++ src/app/recipes/recipes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipes.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes.page.scss'],
 })
 export class RecipesPage implements OnInit {
+
+  recipes: Recipe[] = [{
+    id: 'r1',
+    title: 'Pastas',
+    imageURL: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=612x612',
+    ingredients: ['French Fries', 'Meat', 'Tomatoes']
+  },
+  {
+    id: 'r2',
+    title: 'Sphagiti',
+    imageURL: 'https://media.istockphoto.com/photos/pasta-plate-picture-id632439546?s=612x612',
+    ingredients: ['Sphagetti', 'Meat', 'Tomatoes']
+  }];
 
   constructor() { }
 
